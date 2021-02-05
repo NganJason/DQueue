c1ed1fb45423b9af8fda170ceec8710dffb14bfa
Bang Yi <tanbangyi@gmail.com>
2021-02-05 10:03:52 +0800

    make errors more specific & descriptive

--- backend/handlers/errorHandler.js
+++ backend/handlers/errorHandler.js
@@ -1,9 +1,7 @@
 import ErrorResponse from "../utils/errorResponse.js";
 
 const errorHandler = (err, req, res, next) => {
-  let error = { ...err };
-
-  error.message = err.message;
+  let error = { ...err, message: err.message };
 
   if (err.code === 11000) {
     const message = "Duplicate Field Value Enter";
--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -1,27 +1,22 @@
 import jwt from "jsonwebtoken";
 import { User } from "../models/userModel.js";
-import ErrorResponse from "../utils/errorResponse.js";
+import {UnauthorizedError, NotFoundError} from "../utils/errorResponse.js"
 
 export const isAuthenticated = async (req, res, next) => {
   const token = req.cookies.token;
 
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    var d = new Date();
-
-    if (decoded.exp < d.getTime() / 1000) {
-      return next(new ErrorResponse("Token expired", 400));
-    }
 
     const user = await User.findById(decoded.id);
 
     if (!user) {
-      return next(new ErrorResponse("No user found with this id", 404));
+      return next(new NotFoundError("No user found with this id"));
     }
 
     req.user = user;
     next();
   } catch (error) {
-    return next(new ErrorResponse("Not authorized to access this route", 401));
+    return next(new UnauthorizedError("Not authorized to access this route"));
   }
 };

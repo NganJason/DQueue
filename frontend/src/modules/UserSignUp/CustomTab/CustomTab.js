2e5ce56ef9db77b0c71000d1f4116ed8131078a4
Bang Yi <tanbangyi@gmail.com>
2021-02-06 10:13:24 +0800

    make signup page's tabs reroute

--- frontend/src/modules/UserSignUp/CustomTab/CustomTab.js
+++ frontend/src/modules/UserSignUp/CustomTab/CustomTab.js
@@ -1,5 +1,6 @@
 import React from "react";
 import styles from "./CustomTab.module.scss";
+import { Link } from "react-router-dom";
 
 import { Tabs, Tab } from "@material-ui/core";
 
@@ -11,10 +12,20 @@ const CustomTab = ({ tabIndex, handleChange, tabs }) => {
         onChange={handleChange}
         variant="fullWidth"
         className={styles.tabs}
-        classes={{root: styles.tabsRoot, indicator: styles.tabsIndicator}}
+        classes={{ root: styles.tabsRoot, indicator: styles.tabsIndicator }}
       >
         {tabs.map((tab, index) => (
-          <Tab label={tab} key={index} className={styles.tabStyle} classes={{root: styles.singleTabRoot, wrapper: styles.singleTabWrapper}}/>
+          <Tab
+            key={index}
+            label={tab}
+            className={styles.tabStyle}
+            classes={{
+              root: styles.singleTabRoot,
+              wrapper: styles.singleTabWrapper
+            }}
+            component={Link}
+            to={index == 0 ? "/sign/in" : "/sign/up"}
+          />
         ))}
       </Tabs>
     </>
